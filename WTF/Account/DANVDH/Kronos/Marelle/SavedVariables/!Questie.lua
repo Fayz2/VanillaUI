54e052177fcf51f81c9729d33724b1e02dcbd48a
Fayz2 <97196943+Fayz2@users.noreply.github.com>
2024-06-29 04:51:18 -0400

    configs

---
+++ WTF/Account/DANVDH/Kronos/Marelle/SavedVariables/!Questie.lua
@@ -0,0 +1,331 @@
+
+QuestieSeenQuests = {
+	[2168450461] = 0,
+	[3713273813] = 0,
+	[3017925688] = 0,
+	[2426087176] = 0,
+	[819330568] = 0,
+	[1350591240] = 0,
+	[2162238992] = 0,
+	[1892988520] = 0,
+	[621898218] = 0,
+	[3880462528] = 0,
+	[3543636368] = 0,
+	[3207545725] = 0,
+	[734289709] = 0,
+	[4117710720] = 0,
+	[2083384048] = 0,
+	[3975152101] = 0,
+	[1770499572] = 0,
+	[551826112] = 0,
+	[1832316216] = 0,
+}
+QuestieCachedQuests = {
+	[2168450461] = {
+		["objective1"] = {
+			["notes"] = {
+			},
+			["type"] = "item",
+			["desc"] = " : 0/5",
+		},
+		["logId"] = 14,
+		["objective2"] = {
+			["notes"] = {
+			},
+			["type"] = "item",
+			["desc"] = " : 0/1",
+		},
+		["questName"] = "Treats for Greatfather Winter",
+		["level"] = 60,
+		["tracked"] = false,
+		["leaderboards"] = 2,
+	},
+	[734289709] = {
+		["objective1"] = {
+			["notes"] = {
+			},
+			["type"] = "item",
+			["desc"] = " : 0/3",
+		},
+		["logId"] = 19,
+		["tracked"] = false,
+		["objective2"] = {
+			["notes"] = {
+			},
+			["type"] = "item",
+			["desc"] = " : 0/3",
+		},
+		["objective3"] = {
+			["type"] = "item",
+			["done"] = 1,
+			["notes"] = {
+			},
+			["desc"] = "Warsong Gulch Mark of Honor: 3/3",
+		},
+		["questName"] = "Concerted Efforts",
+		["level"] = 60,
+		["leaderboards"] = 3,
+		["questTag"] = "PvP",
+	},
+	[3017925688] = {
+		["objective1"] = {
+			["notes"] = {
+			},
+			["type"] = "item",
+			["desc"] = " : 0/1",
+		},
+		["logId"] = 17,
+		["objective2"] = {
+			["notes"] = {
+			},
+			["type"] = "item",
+			["desc"] = " : 0/1",
+		},
+		["tracked"] = false,
+		["questName"] = "The Perfect Poison",
+		["level"] = 60,
+		["leaderboards"] = 2,
+		["questTag"] = "Raid",
+	},
+	[2426087176] = {
+		["objective1"] = {
+			["notes"] = {
+			},
+			["type"] = "item",
+			["desc"] = " : 0/1",
+		},
+		["logId"] = 31,
+		["objective2"] = {
+			["type"] = "reputation",
+			["done"] = 1,
+			["notes"] = {
+			},
+			["desc"] = "Zandalar Tribe:  Exalted / Honored",
+		},
+		["questName"] = "Essence Mangoes",
+		["level"] = 60,
+		["tracked"] = false,
+		["leaderboards"] = 2,
+	},
+	[819330568] = {
+		["objective1"] = {
+			["notes"] = {
+			},
+			["type"] = "item",
+			["desc"] = " : 0/10",
+		},
+		["logId"] = 9,
+		["leaderboards"] = 1,
+		["questName"] = "Tabard of the Argent Dawn",
+		["level"] = 60,
+		["tracked"] = false,
+		["questTag"] = "World Event",
+	},
+	[1350591240] = {
+		["tracked"] = false,
+		["questName"] = "Signal for Pickup",
+		["level"] = 52,
+		["leaderboards"] = 0,
+		["logId"] = 11,
+	},
+	[2162238992] = {
+		["tracked"] = false,
+		["leaderboards"] = 0,
+		["questName"] = "Scarlet Subterfuge",
+		["questTag"] = "Elite",
+		["logId"] = 27,
+		["level"] = 60,
+	},
+	[1892988520] = {
+		["objective1"] = {
+			["notes"] = {
+			},
+			["type"] = "item",
+			["desc"] = "Blasted Boar Lung: 0/3",
+		},
+		["logId"] = 2,
+		["objective2"] = {
+			["notes"] = {
+			},
+			["type"] = "item",
+			["desc"] = "Scorpok Pincer: 0/2",
+		},
+		["objective3"] = {
+			["notes"] = {
+			},
+			["type"] = "item",
+			["desc"] = "Basilisk Brain: 0/1",
+		},
+		["questName"] = "Spirit of the Boar",
+		["level"] = 50,
+		["tracked"] = false,
+		["leaderboards"] = 3,
+	},
+	[621898218] = {
+		["tracked"] = false,
+		["leaderboards"] = 0,
+		["questName"] = "The Treasure of the Shen'dralar",
+		["questTag"] = "Dungeon",
+		["logId"] = 5,
+		["level"] = 60,
+	},
+	[3880462528] = {
+		["objective1"] = {
+			["type"] = "event",
+			["done"] = 1,
+			["notes"] = {
+			},
+			["desc"] = "Escort Galen out of the Fallow Sanctuary.",
+		},
+		["logId"] = 23,
+		["isComplete"] = 1,
+		["questName"] = "Galen's Escape",
+		["level"] = 38,
+		["tracked"] = false,
+		["leaderboards"] = 1,
+	},
+	[3543636368] = {
+		["objective1"] = {
+			["notes"] = {
+			},
+			["type"] = "item",
+			["desc"] = " : 0/1",
+		},
+		["leaderboards"] = 1,
+		["questName"] = "The Stone Circle",
+		["level"] = 51,
+		["logId"] = 25,
+		["tracked"] = false,
+	},
+	[3207545725] = {
+		["tracked"] = false,
+		["leaderboards"] = 0,
+		["questName"] = "Feathermoon Stronghold",
+		["questTag"] = "Dungeon",
+		["logId"] = 4,
+		["level"] = 57,
+	},
+	[3975152101] = {
+		["tracked"] = false,
+		["questName"] = "The Reason for the Season",
+		["level"] = 60,
+		["leaderboards"] = 0,
+		["logId"] = 13,
+	},
+	[4117710720] = {
+		["tracked"] = false,
+		["leaderboards"] = 0,
+		["questName"] = "Tabetha's Task",
+		["questTag"] = "Dungeon",
+		["logId"] = 29,
+		["level"] = 46,
+	},
+	[2083384048] = {
+		["objective1"] = {
+			["notes"] = {
+			},
+			["type"] = "monster",
+			["desc"] = "  slain: 0/1",
+		},
+		["logId"] = 16,
+		["leaderboards"] = 1,
+		["questName"] = "Azure Templar",
+		["level"] = 60,
+		["tracked"] = false,
+		["questTag"] = "Elite",
+	},
+	[551826112] = {
+		["tracked"] = false,
+		["leaderboards"] = 0,
+		["questName"] = "Nat's Measuring Tape",
+		["questTag"] = "Raid",
+		["logId"] = 32,
+		["level"] = 60,
+	},
+	[3713273813] = {
+		["objective1"] = {
+			["notes"] = {
+			},
+			["type"] = "item",
+			["desc"] = " : 0/1",
+		},
+		["logId"] = 21,
+		["objective2"] = {
+			["notes"] = {
+			},
+			["type"] = "item",
+			["desc"] = " : 0/1",
+		},
+		["questName"] = "A Noble Brew",
+		["level"] = 30,
+		["tracked"] = false,
+		["leaderboards"] = 2,
+	},
+	[1832316216] = {
+		["objective1"] = {
+			["notes"] = {
+			},
+			["type"] = "item",
+			["desc"] = " : 0/1",
+		},
+		["logId"] = 6,
+		["leaderboards"] = 1,
+		["questName"] = "Unfinished Gordok Business",
+		["level"] = 60,
+		["tracked"] = false,
+		["questTag"] = "Dungeon",
+	},
+	[1770499572] = {
+		["objective1"] = {
+			["notes"] = {
+			},
+			["type"] = "item",
+			["desc"] = " : 0/8",
+		},
+		["logId"] = 8,
+		["leaderboards"] = 1,
+		["questName"] = "Blessed Wizard Oil",
+		["level"] = 60,
+		["tracked"] = false,
+		["questTag"] = "World Event",
+	},
+}
+QuestieConfig = {
+	["trackerAlpha"] = 0.6,
+	["alwaysShowObjectives"] = true,
+	["maxShowLevel"] = 7,
+	["showToolTips"] = true,
+	["clusterQuests"] = true,
+	["getVersion"] = "3.7.1",
+	["showMapNotes"] = true,
+	["boldColors"] = false,
+	["showTrackerHeader"] = false,
+	["trackerScale"] = 1,
+	["arrowEnabled"] = true,
+	["trackerMinimize"] = false,
+	["trackerList"] = false,
+	["arrowTime"] = true,
+	["useQuestLinks"] = true,
+	["trackerBackground"] = false,
+	["trackerEnabled"] = true,
+	["resizeWorldmap"] = false,
+	["showProfessionQuests"] = false,
+	["minLevelFilter"] = true,
+	["minimapZoom"] = false,
+	["hideMinimapIcons"] = false,
+	["minShowLevel"] = 4,
+	["maxLevelFilter"] = true,
+	["corpseArrow"] = true,
+	["minimapButton"] = false,
+	["hideObjectives"] = false,
+}
+QuestieTrackerVariables = {
+	["position"] = {
+		["relativeTo"] = "UIParent",
+		["point"] = "CENTER",
+		["relativePoint"] = "CENTER",
+		["yOfs"] = 0,
+		["xOfs"] = 0,
+	},
+}
+QuestieMinimapPosition = -90
