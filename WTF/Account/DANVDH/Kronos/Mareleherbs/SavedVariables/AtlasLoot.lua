af6fb54caa60daf4bbfc89eec7870d271dabf023
Fayz2 <97196943+Fayz2@users.noreply.github.com>
2024-09-30 12:44:22 -0400

    add Mareleherbs

---
+++ WTF/Account/DANVDH/Kronos/Mareleherbs/SavedVariables/AtlasLoot.lua
@@ -0,0 +1,28 @@
+
+AtlasLootCharDB = {
+	["DefaultTT"] = true,
+	["ItemSyncTT"] = false,
+	["AutoQuery"] = false,
+	["ItemIDs"] = false,
+	["LastBossText"] = "Rethilgore",
+	["Opaque"] = false,
+	["SafeLinks"] = true,
+	["SearchResult"] = {
+	},
+	["PartialMatching"] = true,
+	["MinimapButtonRadius"] = 88,
+	["WishList"] = {
+	},
+	["LootlinkTT"] = false,
+	["HidePanel"] = false,
+	["ItemSpam"] = true,
+	["EquipCompare"] = false,
+	["AllLinks"] = false,
+	["QuickLooks"] = {
+	},
+	["AtlasLootVersion"] = "40605",
+	["MinimapButtonPosition"] = 308,
+	["FirstTime"] = false,
+	["MinimapButton"] = false,
+	["LastBoss"] = "SFKRethilgore",
+}
